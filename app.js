da37b9049958c252174329e14fd716b87d6d5000
naorpeled <thehecticbyte@gmail.com>
2020-02-21 01:06:20 +0200

    Added canvas saving - Now the current canvas is shown to new users

--- app.js
+++ app.js
@@ -3,25 +3,29 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 let clients = [];
+let currentCanvas = null;
 
 io.on('connection', function(socket){
+  socket.emit('initialCanvasLoad', currentCanvas);
   socket.on('onClientConnect', function(name) {
     console.log(`A user named ${name} has connected.`);
     clients.push(name);
     io.emit('onClientConnect', clients);
   });
-  
+
   socket.on('onClientDisconnect', function(name){
     clients.splice(clients.indexOf(name), 1); 
     io.emit('onClientDisconnect', clients);
   });
 
   socket.on('onDraw', function(data) {
+    currentCanvas = data.canvas;
     io.emit('onDraw', data);
   });
 
   socket.on('onCanvasClear', () => {
-    console.log('clearning canvas');
+    console.log('Clearning canvas...');
+    currentCanvas = null;
     io.emit('onCanvasClear');
   })
 });
