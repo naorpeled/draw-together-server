c6f713a59b11835467afbb77435463ce1a172919
naorpeled <thehecticbyte@gmail.com>
2020-02-20 22:15:49 +0200

    Added support for client drawing - Now listening to onDraw events from the client

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const io = require('socket.io')(server);
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('onDraw', function(data) {
+    socket.broadcast.emit('onDraw', data);
+  });
 });
 
 server.listen(8000, function(){
