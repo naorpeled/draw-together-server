3af724e7f83210ab2c01633c05aa35c5fcfae545
naorpeled <thehecticbyte@gmail.com>
2020-02-20 22:16:51 +0200

    Added support for canvas clearing - Now listening to the canvas clear event from the client

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ io.on('connection', function(socket){
   socket.on('onDraw', function(data) {
     socket.broadcast.emit('onDraw', data);
   });
+  socket.on('onCanvasClear', () => {
+    console.log('clearning canvas');
+    socket.broadcast.emit('onCanvasClear');
+  })
 });
 
 server.listen(8000, function(){
