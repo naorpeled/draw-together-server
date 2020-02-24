52e4fbe47357832fa10a06c3946160ba9b720eef
naorpeled <thehecticbyte@gmail.com>
2020-02-24 12:29:35 +0200

    Added rescaling/resizing support - When a user resizes the page, the server will now send the most updated canvas. In the near future I might adjust it a bit.

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+// KEY     id
+// VALUE   clients, canvas, messages
 let rooms = new Map();
-// id, clients, canvas, messages
 
 io.on('connection', function(socket) {
   const roomId = socket.handshake.query.roomId;
@@ -16,6 +17,7 @@ io.on('connection', function(socket) {
     });
   }
   let roomInfo = rooms.get(roomId);
+
   socket.on('onClientConnect', function(name) {
     console.log(`A user named ${name} has connected to room #${roomId}.`);
 
@@ -29,7 +31,12 @@ io.on('connection', function(socket) {
       id: socket.id
     });
 
-    io.to(roomId).emit('onClientConnect', info.clients, info.messages);
+    const data = {
+      users: info.clients,
+      messages: info.messages
+    }
+
+    io.to(roomId).emit('onClientConnect', data);
   });
 
   socket.on('onDraw', function(data) {
@@ -54,6 +61,13 @@ io.on('connection', function(socket) {
     info.messages.push(data);
     socket.to(roomId).emit('onChatMessage', data);
   })
+
+  socket.on('onClientRescale', (data) => {
+    let info = roomInfo;
+    data.canvas = info.canvas;
+    
+    socket.emit('onClientRescale', data);
+  });
 
   socket.on('disconnect', () => {
     let info = roomInfo;
